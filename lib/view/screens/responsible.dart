fb3e374d25756d7dfe3330b99fabcfd4b964b181
ahmed <ahmed.elsaba277@gmail.com>
2022-11-24 17:03:38 +0200

    parent

---
+++ lib/view/screens/responsible.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/material.dart';
+
+class ResponsiblePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
